d90bee725dbefb67cfd085fdd95f84c73f949910
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 <Vivekpatil254@gmail.com>
2022-05-30 00:53:19 +0530

    UC3 Commit

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,19 +1,76 @@
 package com.bridgelabz;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program ...");
-
-        Contact contact = new Contact();
+        Scanner scanner=new Scanner(System.in);
+        int choice,flag=0;
+        String name;
+        Contact contact = new Contact();;
         List<Contact> addressBook = new ArrayList<>();
-        contact.getdata();
-        addressBook.add(contact);
-        Contact temp = addressBook.get(0);
-        temp.display();
+    while (true) {
 
+
+        System.out.println("1. ADD CONTACT");
+        System.out.println("2. EDIT CONTACT");
+        System.out.println("3. DISPLAY CONTACT");
+        System.out.println("4. EXIT");
+        System.out.println("Enter your choice --> ");
+        choice =scanner.nextInt();
+        switch(choice)
+        {
+            case 1:
+                System.out.println("Enter first name     : ");
+                name =scanner.next();
+                contact.getdata(name);
+                addressBook.add(contact);
+                break;
+            case 2:
+                System.out.println("Enter first name to edit     : ");
+                name =scanner.next();
+//                addressBook.stream().forEach(contact1 -> {
+//                    if(name.equalsIgnoreCase(contact1.getFirstName()))
+//                        contact1.display();
+//                });
+                for (Contact contact1:addressBook) {
+                    if(contact1.getFirstName().equalsIgnoreCase(name))
+                    {
+                        contact1.getdata(name);
+                        flag=1;
+                    }
+                }
+                if(flag == 1)
+                {
+                    System.out.println("Data Edited Successfully...");
+                    flag=0;
+                    }
+                else
+                {
+                    System.out.println("the entered contact not present in addressbook");
+                    System.out.println("enter data for new contact in addressbook");
+                    contact.getdata(name);
+                }
+                break;
+            case 3:
+                for (Contact contact1:addressBook) {
+                    contact1.display();
+                }
+                break;
+            case 4:
+                System.exit(0);
+        }
+
+
+
+
+
+    }
     }
 }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -7,11 +7,83 @@ public class Contact {
 
     String firstName,state,lastName,address,city,email;
     long zip,phoneNumber,addressBookIndex;
-    public void getdata()
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getAddressBookIndex() {
+        return addressBookIndex;
+    }
+
+    public void setAddressBookIndex(long addressBookIndex) {
+        this.addressBookIndex = addressBookIndex;
+    }
+
+    public void getdata(String firstName)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first name     :    ");
-        firstName=scanner.nextLine();
+        this.firstName = firstName;
         System.out.println("Enter last name     :    ");
         lastName=scanner.nextLine();
         System.out.println("Enter address       :    ");
