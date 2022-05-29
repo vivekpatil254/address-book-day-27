9ecaaacd20e1f99587a07e505df3446e008bd051
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 <Vivekpatil254@gmail.com>
2022-05-29 21:49:37 +0530

    UC2 Commit

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,12 +1,19 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program ...");
 
         Contact contact = new Contact();
+        List<Contact> addressBook = new ArrayList<>();
         contact.getdata();
-        contact.display();
+        addressBook.add(contact);
+        Contact temp = addressBook.get(0);
+        temp.display();
+
     }
 }
