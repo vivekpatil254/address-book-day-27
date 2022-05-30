6158836f235efe28447245cbc6426449040c2226
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 <Vivekpatil254@gmail.com>
2022-05-30 10:44:35 +0530

    UC4 Commit

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -13,32 +13,37 @@ public class AddressBookMain {
         Scanner scanner=new Scanner(System.in);
         int choice,flag=0;
         String name;
-        Contact contact = new Contact();;
+        Contact contact = new Contact();
+        Contact contact2 = new Contact();
         List<Contact> addressBook = new ArrayList<>();
+      //  contact.getdata("aaaa");
+      //  contact2.getdata("bbb");
+//        addressBook.add(contact);
+//        addressBook.add(contact2);
+
     while (true) {
 
 
         System.out.println("1. ADD CONTACT");
         System.out.println("2. EDIT CONTACT");
-        System.out.println("3. DISPLAY CONTACT");
-        System.out.println("4. EXIT");
+        System.out.println("3. DELETE CONTACT");
+        System.out.println("4. DISPLAY CONTACT");
+        System.out.println("5. EXIT");
         System.out.println("Enter your choice --> ");
         choice =scanner.nextInt();
         switch(choice)
         {
             case 1:
+                Contact temp = new Contact();
                 System.out.println("Enter first name     : ");
                 name =scanner.next();
-                contact.getdata(name);
-                addressBook.add(contact);
+                temp.getdata(name);
+                addressBook.add(temp);
                 break;
             case 2:
+                Contact temp = new Contact();
                 System.out.println("Enter first name to edit     : ");
                 name =scanner.next();
-//                addressBook.stream().forEach(contact1 -> {
-//                    if(name.equalsIgnoreCase(contact1.getFirstName()))
-//                        contact1.display();
-//                });
                 for (Contact contact1:addressBook) {
                     if(contact1.getFirstName().equalsIgnoreCase(name))
                     {
@@ -58,12 +63,39 @@ public class AddressBookMain {
                     contact.getdata(name);
                 }
                 break;
-            case 3:
+            case 3 :
+                System.out.println("Enter first name to edit     : ");
+                name =scanner.next();
+               // Contact contact2;
+//                addressBook.stream().forEach(contact1 -> {
+//                    if(name.equalsIgnoreCase(contact1.getFirstName()))
+//                        contact1.display();
+//                });
+                for (Contact contact1:addressBook) {
+                    if(contact1.getFirstName().equalsIgnoreCase(name))
+                    {   contact2=contact1;
+                      //  addressBook.remove(contact1);
+                        flag=1;
+                    }
+                }
+                if(flag == 1)
+                {   addressBook.remove(contact2);
+                    System.out.println("Data Delete Seccessfully...");
+                    flag=0;
+                }
+                else
+                {
+                    System.out.println("the entered contact not present in addressbook");
+                    System.out.println("enter data for new contact in addressbook");
+                    contact.getdata(name);
+                }
+                break;
+            case 4:
                 for (Contact contact1:addressBook) {
                     contact1.display();
                 }
                 break;
-            case 4:
+            case 5:
                 System.exit(0);
         }
 
